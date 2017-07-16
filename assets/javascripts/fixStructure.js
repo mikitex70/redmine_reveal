a5e3be0eecbb52d21d52118d3129d1385381e644
Michele Tessaro <michele.tessaro@email.it>
2017-07-16 17:16:48 +0200

    fix: dev: fixed fragment classes with Redmine 3.4.0+

--- assets/javascripts/fixStructure.js
+++ assets/javascripts/fixStructure.js
@@ -1,4 +1,12 @@
 (function() {
+    // Fix for Redmine 3.4.0+: fragment classes have a prefix
+    $("span[class^='wiki-class-']").each(function() {
+        var el = $(this);
+        var fixedClasses = el.attr("class").replace(/wiki-class-/g, "");
+        
+        el.attr("class", fixedClasses);
+    });
+    
     function moveSlideDefinitions() {
         var lastSection = null,
             lastSlide   = null;
