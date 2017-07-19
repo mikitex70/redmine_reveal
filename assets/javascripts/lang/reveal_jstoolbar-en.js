75634bfc7726a353ed89dd3c65e50e7ca9c510b5
Michele Tessaro <michele.tessaro@email.it>
2017-07-19 18:22:10 +0200

    new: usr: added some missing string translations

--- assets/javascripts/lang/reveal_jstoolbar-en.js
+++ assets/javascripts/lang/reveal_jstoolbar-en.js
@@ -1,4 +1,6 @@
-RMReveal = RMReveal || {};
+if(typeof(RMReveal) === 'undefined')
+    RMReveal = { strings: {} };
+
 RMReveal.strings['reveal_presentation_start' ] = 'Presentation';
 RMReveal.strings['reveal_btn_ok'             ] = 'Insert macro';
 RMReveal.strings['reveal_btn_cancel'         ] = 'Cancel';
@@ -10,3 +12,4 @@ RMReveal.strings['reveal_slidesetup_btntitle'] = 'Presentation setup';
 RMReveal.strings['reveal_slidesetup_dlgtitle'] = 'Global presentation settings';
 RMReveal.strings['reveal_fragment_btntitle'  ] = 'Apply animation effect to text';
 RMReveal.strings['reveal_backsize_validation'] = "Specify a number with suffix 'px', 'mm', 'cm' or 'in', or one of  'auto', 'cover', 'contain'"
+RMReveal.strings['reveal_invalid_input'      ] = 'Please check your input.';
